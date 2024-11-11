c3015f37dda10c8ec86c16ff603402081a0efe23
dengzhipenglove <2877400465@qq.com>
2024-11-11 11:04:33 +0800

    Update extract_const_test.go

--- extract_const_test.go
+++ extract_const_test.go
@@ -10,18 +10,14 @@ import (
 
 func TestExtractConst(t *testing.T) {
 
-	pkgName, constdata, err := ExtractGoFileConst("./test_data.go", "")
+	pkgName, constdata, err := ExtractGoFileConst("./test_data.go")
 	assert.Empty(t, err)
 	pretty.Println(pkgName)
 	pretty.Println(constdata)
 
 	for _, v := range constdata {
 		if v.Name == "Const_String_Data" {
-			fmt.Printf("ValueString:[%s],len:%d\n", v.ValueString, len(v.ValueString))
-			if v.ValueString == "str1111\"" {
-				fmt.Println("Equal")
-			}
+			assert.Equal(t, v.ValueString, "str1111\"")
 		}
-
 	}
 }
