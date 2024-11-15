6441e9e05c709f89f88235dbd50a0b01048b0599
dengzhipeng <1877400465@qq.com>
2024-11-15 15:05:42 +0800

    test case

--- extract_const_test.go
+++ extract_const_test.go
@@ -3,20 +3,19 @@ package goextract
 import (
 	"testing"
 
-	"github.com/kr/pretty"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestExtractConst(t *testing.T) {
 
-	pkgName, constdata, err := ExtractGoFileConst("./test_data.go")
-	assert.Empty(t, err)
-	pretty.Println(pkgName)
-	pretty.Println(constdata)
+	_, constdata, err := ExtractGoFileConst("./test_data.go")
+	require.NoError(t, err)
+	//pretty.Println(pkgName)
+	//pretty.Println(constdata)
 
 	for _, v := range constdata {
 		if v.Name == "Const_String_Data" {
-			assert.Equal(t, v.ValueString, "str1111\"")
+			require.Equal(t, v.ValueString, "str1111\"")
 		}
 	}
 }
